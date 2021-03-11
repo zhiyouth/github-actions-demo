38ba364d1d7a9c37f2b9a9f85c8260ec1d618c63
tanghao <tanghao@MacBook-Pro.local>
2021-03-11 16:40:46 +0800

    feat: init

---
+++ src/components/NavigationBar/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import './index.less';
+class NavigationBar extends Component {
+    render () {
+        return (
+            <div className="navigation-bar">
+                NavigationBar
+            </div>
+        );
+    }
+}
+export default NavigationBar;
