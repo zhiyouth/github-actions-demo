0801b5c71223a53c93bf7fb602683a6cee26f6cf
zhiyouth <49746677+zhiyouth@users.noreply.github.com>
2021-03-18 09:18:47 +0000

    Deploying to gh-pages from  @ c44d2e426a7c8f21b893e71edd5b2a0f4467e97c 🚀

---
+++ precache-manifest.009126a78388f6dd4902e067ba0ae3c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f7067b05406673053a0fc8f602cb85",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "9f9929db1b35ffd55c46",
+    "url": "/github-actions-demo/static/js/2.dece54db.chunk.js"
+  },
+  {
+    "revision": "c4437eeb8965c4233c95",
+    "url": "/github-actions-demo/static/js/main.d609755a.chunk.js"
+  },
+  {
+    "revision": "41870333dfc813cf444c",
+    "url": "/github-actions-demo/static/js/runtime~main.dd1e6690.js"
+  },
+  {
+    "revision": "bf572724aa0cd5f60c1c2b247daac1f9",
+    "url": "/github-actions-demo/static/media/close.bf572724.jpeg"
+  }
+]);
